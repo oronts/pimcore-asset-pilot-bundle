7a59a5b044f2fe862d6f7263ef87c391db55aafc
Refaat Al Ktifan <Refaat@alktifan.com>
2026-07-28 18:53:38 +0200

    Add prebuilt Studio remote to the release tag

---
+++ public/studio/build/2.0.0-add0da7d-a54a-43a7-93d8-e874834782eb/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.oronts_asset_pilot_bundle = "/bundles/orontsassetpilot/studio/build/2.0.0-add0da7d-a54a-43a7-93d8-e874834782eb/static/js/remoteEntry.js"
+
+      
+    
